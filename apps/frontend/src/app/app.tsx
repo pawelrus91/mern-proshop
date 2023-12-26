1b672be1dc96c667be1274a9831251051804b726
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2023-12-26 18:02:16 +0100

    chore: clean up

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,14 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import React from 'react';
 
-import NxWelcome from './nx-welcome';
-
-export function App() {
-  return (
-    <div>
-      <NxWelcome title="frontend" />
-    </div>
-  );
-}
+const App = () => {
+  return <h1>Welcome To ProShop</h1>;
+};
 
 export default App;
