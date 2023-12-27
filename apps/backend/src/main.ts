6b3296dd92903ce25fc9f258bf71f7107ce65096
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2023-12-27 19:26:43 +0100

    chore: setup express with fake product response

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,34 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+
+import { products } from '@mern-proshop/data';
+
+const app = express();
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to backend!' });
+});
+
+app.get('/api/products', (req, res) => {
+  res.send(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(
+    (productEntity) => productEntity._id === req.params.id
+  );
+  res.send(product);
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
